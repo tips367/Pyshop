174ecf6384164719bdd047d4058a85f097f43682
tips367 <tipu367.khan@hotmail.com>
2019-04-13 22:22:54 +0530

    Adding PyShop project files

---
+++ products/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Products, Offer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code','discount')
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
